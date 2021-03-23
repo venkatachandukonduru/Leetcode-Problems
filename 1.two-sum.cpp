6f4932c8481dc1f6bfe985eeb5fc27337ff8c655
venkatachandukonduru <venkatachandukonduru@gmail.com>
2021-03-23 16:33:07 +0530

    First Commit

---
+++ 1.two-sum.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     unordered_map<int, int> indices;
+        for (int i = 0; i < nums.size(); i++) {
+            if (indices.find(target - nums[i]) != indices.end()) {
+                return {indices[target - nums[i]], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
+
