8605ca63e4bb924dc409a97d0c25b1fe369a09d2
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2021-07-14 19:07:06 +0530

    Time: 8 ms (88.00%), Space: 5.8 MB (70.64%) - LeetHub

---
+++ power-of-three/power-of-three.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int var=1;
+    int func(int n)
+    {
+        if(n==3||n==1)return 1;
+        if(n==0)return 0;
+        if(n%3!=0){var=0;return 0;}
+        return func(n/3);
+        
+    }
+    bool isPowerOfThree(int n) {
+        return var and func(n);
+    }
+};
+/*
+if(n%2==0)return 0;
+        while(n>=1)
+        {
+            if(!(n%3==0))return 0;
+            n/=3;
+        }
+        return 1;
+        */
