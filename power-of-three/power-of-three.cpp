edfc9bb4906c878aa617f3a16387f2608abf8bc5
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2021-07-14 19:21:32 +0530

    Time: 16 ms (51.58%), Space: 5.9 MB (29.49%) - LeetHub

--- power-of-three/power-of-three.cpp
+++ power-of-three/power-of-three.cpp
@@ -4,8 +4,11 @@ public:
     int func(int n)
     {
         if(n==3||n==1)return 1;
-        if(n==0)return 0;
-        if(n%3!=0){var=0;return 0;}
+        if(n==0||n%3!=0)
+        {
+            var=0;
+            return 0;
+        }
         return func(n/3);
         
     }
