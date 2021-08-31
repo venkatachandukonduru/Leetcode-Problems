9f82932ae3cc3e4745e7eebe8348dbba9ba1c4da
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2021-09-01 00:27:19 +0530

    Time: 6 ms (16.93%), Space: 6.2 MB (69.81%) - LeetHub

--- backspace-string-compare/backspace-string-compare.cpp
+++ backspace-string-compare/backspace-string-compare.cpp
@@ -5,7 +5,7 @@ public:
             if(i>=1 and s[i]=='#' )s.erase(s.begin()+i-1),s.erase(s.begin()+i-1),i-=2;
         for(int i=0;i<t.size();i++)
             if(i>=1 and t[i]=='#' )t.erase(t.begin()+i-1),t.erase(t.begin()+i-1),i-=2;
-        cout<<"S:"<<s<<"  T:"<<t;
+        //cout<<"S:"<<s<<"  T:"<<t;
         while(s[0]=='#')s.erase(s.begin());
         while(t[0]=='#')t.erase(t.begin());
         return s==t;
