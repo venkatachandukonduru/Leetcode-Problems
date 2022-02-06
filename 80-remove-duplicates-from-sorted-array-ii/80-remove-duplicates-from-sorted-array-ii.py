82bf664e42f764601c435466d8b47aa3bd2d492e
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-02-06 11:53:04 +0530

    Time: 122 ms (5.80%), Space: 13.9 MB (96.82%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -3,12 +3,8 @@ class Solution:
         if len(nums)<3:
             return len(nums)
         i = 2
-        z = len(nums)
         while i<len(nums):
-            if nums[i]==nums[i-1] and nums[i-1] ==nums[i-2]:
-                print(i)
-                z-=1
-                del nums[i]
+            if nums[i]==nums[i-1] == nums[i-1] ==nums[i-2]: del nums[i]
             else: i+=1
         return len(nums)
             
