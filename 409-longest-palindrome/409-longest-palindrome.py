30352ce22da59c531b205acc3c27a0b27bc14406
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-02-19 00:16:42 +0530

    Time: 61 ms (16.43%), Space: 13.8 MB (89.70%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,9 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
+        count = flag = 0
+        for h,k in Counter(s).items() :
+            if k%2 != 0 : flag = 1
+            count+=k if k%2==0 else k-1
+        return count+flag
+        '''
         j = Counter(s)
+        #for h,k in Counter(s).items(): print(h,k)
         flag = count = 0
         for p in j:
             if j[p]%2!=0: flag = 1
             count += j[p] if j[p]%2==0 else j[p]-1
-            print(count)
         return count+flag if count%2 == 0 else count
+        '''
