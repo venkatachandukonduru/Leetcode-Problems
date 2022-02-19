8b96543ea19fcd7b3690b1b333d53cfd3040818d
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-02-20 01:40:57 +0530

    Time: 34 ms (70.53%), Space: 13.8 MB (96.79%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, p: str, s: str) -> bool:
+        p = [x for x in p]
+        s = s.split(" ")
+        if len(p) != len(s) : return False
+        dic1 = {}
+        for i in range(len(p)):
+            if dic1.get(p[i],None)!=s[i]:
+                if dic1.get(p[i],None) == None : dic1[p[i]] = s[i]
+                else : return False
+        for r,t in dic1.items():
+            dic1[r] = 9
+            if t in dic1.values() : return False
+        print(p,s,dic1)
+        return True
