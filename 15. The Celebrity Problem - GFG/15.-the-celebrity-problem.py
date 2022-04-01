7bdda0313dbd14e508281b1571c91cd617cc690d
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-04-01 12:03:15 +0530

    Added solution - LeetHub

---
+++ 15. The Celebrity Problem - GFG/15.-the-celebrity-problem.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find if there is a celebrity in the party or not.
+    def celebrity(self, m, n):
+        if sum(map(sum,m))==0: return -1
+        for i in range(n):
+            if sum(m[i])==0: return i
+        return -1
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        k = 0
+        m = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                row.append(a[k])
+                k+=1
+            m.append(row)
+        ob = Solution()
+        print(ob.celebrity(m,n))
+# } Driver Code Ends
