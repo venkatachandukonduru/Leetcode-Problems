bc84767a39e9cf0dee38af15b5d210789474f073
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-04-25 20:38:29 +0530

    Time: 59 ms (18.23%), Space: 13.9 MB (77.87%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -7,15 +7,16 @@
 class Solution:
     def binaryTreePaths(self, root) -> List[str]:
         self.res = []
+        
         def traversal(self,root,list1):
-            if root is None:
-                return 
+            if not root: return 
             if not root.left and not root.right:
-                list1+=str(root.val)
-                self.res.append(list1)
+                self.res.append(list1+str(root.val))
             return traversal(self,root.left,list1+str(root.val)+"->") or traversal(self,root.right,list1+str(root.val)+"->")
+        
         traversal(self,root,"")
         return self.res
+        
             
         
         
