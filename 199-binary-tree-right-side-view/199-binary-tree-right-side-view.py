c105035b9fc061ee087399d1063edcfb6e48c3de
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-09-04 15:53:14 -0700

    Time: 47 ms (64.21%), Space: 13.9 MB (70.62%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -7,8 +7,7 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         #Using BFS
-        res = []
-        temp = [root]
+        temp,res = [root],[]
         while any(temp):
             res.append(temp[-1].val)
             temp = [ child for node in temp for child in [node.left,node.right] if child]
