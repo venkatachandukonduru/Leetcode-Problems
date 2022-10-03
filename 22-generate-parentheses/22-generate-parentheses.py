8cc78c36134ea70a1e35d23e8d1758dc215c580e
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-10-02 19:27:51 -0700

    Time: 73 ms (23.69%), Space: 14.2 MB (39.60%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,7 +3,7 @@ class Solution:
         res = []
         def add_par(temp,m,n,p):
             if m>p/2 or n>p/2 or n > m or len(temp) > p: return
-            if m==p/2 and n==p/2 and len(temp) == p: res.append(temp)
+            if len(temp) == p: res.append(temp)
             
             add_par(temp+'(',m+1,n,p)
             add_par(temp+')',m,n+1,p)
