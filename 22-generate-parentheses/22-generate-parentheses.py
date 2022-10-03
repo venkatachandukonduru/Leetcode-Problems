d90fadb6512772ce0cf404ed72a531e85bfa4d57
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-10-02 19:27:33 -0700

    Time: 88 ms (8.94%), Space: 14.3 MB (39.60%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -2,7 +2,7 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
         def add_par(temp,m,n,p):
-            if n > m or len(temp) > p: return
+            if m>p/2 or n>p/2 or n > m or len(temp) > p: return
             if m==p/2 and n==p/2 and len(temp) == p: res.append(temp)
             
             add_par(temp+'(',m+1,n,p)
