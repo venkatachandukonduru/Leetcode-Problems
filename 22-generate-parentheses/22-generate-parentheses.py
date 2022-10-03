7a543f76db4d1366d1ed824d97faf503b226fc50
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-10-02 21:09:24 -0700

    Time: 42 ms (84.13%), Space: 14.3 MB (11.71%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,14 +1,53 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def add_par(temp,m,n,p):
-            if n > m or m>p/2 or n>p/2: return
-            if len(temp) == p: res.append(temp)
-            
-            add_par(temp+'(',m+1,n,p)
-            add_par(temp+')',m,n+1,p)
+class Solution(object):
+    def generateParenthesis(self, n):
+        res,temp =set(),["()"]
+        for i in range(2,n+1):
+            for x in temp:
+                for i in range(len(x)):
+                    res.add(x[:i]+"()"+x[i:])
+                    # print(x[:i]+"()"+x[i+1:])
+            temp = [x for x in res]
+            res.clear()
+        return temp
+                
         
-        p = n*2
-        add_par("",0,0,p)
-        return res
+#         #Brute Force
+#         #Didnt Understand! Look Again
+#         def generate(A = []):
+#             if len(A) == 2*n:
+#                 if valid(A):
+#                     ans.append("".join(A))
+#             else:
+#                 A.append('(')
+#                 generate(A)
+#                 A.pop()
+#                 A.append(')')
+#                 generate(A)
+#                 A.pop()
+#                 print(A)
+#         def valid(A):
+#             bal = 0
+#             for c in A:
+#                 if c == '(': bal += 1
+#                 else: bal -= 1
+#                 if bal < 0: return False
+#             return bal == 0
+
+#         ans = []
+#         generate()
+#         return ans
+
+# class Solution:
+#     def generateParenthesis(self, n: int) -> List[str]:
+#         res = []
+#         def add_par(temp,m,n,p):
+#             if n > m or m>p/2 or n>p/2: return
+#             if len(temp) == p: res.append(temp)
+            
+#             add_par(temp+'(',m+1,n,p)
+#             add_par(temp+')',m,n+1,p)
+        
+#         p = n*2
+#         add_par("",0,0,p)
+#         return res
             
