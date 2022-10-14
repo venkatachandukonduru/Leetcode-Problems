e0beb7c7a91e4adc4b8e494bcb1e74f5c4c6ee48
Konduru Venkata Chandu <58496589+venkatachandukonduru@users.noreply.github.com>
2022-10-14 14:02:13 -0700

    Time: 6416 ms (7.33%), Space: 14.1 MB (30.99%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        z = "".join(sorted(s1))
+        for i in range(len(s2)-len(s1)+1):
+            if "".join(sorted(s2[i:i+len(z)])) == z:return True
+        return False
+            
